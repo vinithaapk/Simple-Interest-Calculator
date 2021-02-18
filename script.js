ecc06ef543be06e1c2a12b4191258c1bbe20df4b
Vinitha <vinithaapk@gmail.com>
2021-02-18 13:30:32 +0530

    Add existing file

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
+function updateTextInput()
 {
-    p = document.getElementById("principal").value;
-    
+var slider = document.getElementById("r");
+var output = document.getElementById("rate");
+output.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
-        
+}
+function compute()
+
+{
+    p = document.getElementById("p").value;
+    n = document.getElementById("n").value;
+    r = document.getElementById("r").value;
+    result = document.getElementById("result");
+                
+    result.innerHTML = "The interest is " + (p*n*r/100);
+}
